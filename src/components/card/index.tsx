e7e54db3fd6b3e513ebac6bc87004cb0a726bba6
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2020-09-03 20:59:18 -0300

    Complete Component Cards

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,1 +1,20 @@
 import React from 'react'
+
+interface CardProps {
+    title: string,
+    text: string
+}
+
+const Card: React.FC<CardProps> = (props) => {
+    return (
+        <section className="card">
+            <section className="card__imageContent">
+                {props.children}
+            </section>
+            <h4 className="card__title">{props.title}</h4>
+            <p className="card__text">{props.text}</p>
+        </section>
+    )
+}
+
+export default Card
--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import BannerTopHome from '../../components/banner'
 import BannerImageMobile from '../../assets/images/image-intro-mobile.jpg'
 import BannerImageDesktop from '../../assets/images/image-intro-desktop.jpg'
+import Card01Image from '../../assets/images/icon-snappy-process.svg'
+import Card02Image from '../../assets/images/icon-affordable-prices.svg'
+import Card03Image from '../../assets/images/icon-people-first.svg'
+import Card from '../../components/card'
 
 const Landing = () => {
     return (
@@ -14,6 +18,19 @@ const Landing = () => {
                 <img src={BannerImageMobile} alt="Humanizing your insurance" className="banner_image--mobile"/>
                 <img src={BannerImageDesktop} alt="Humanizing your insurance" className="banner_image--desktop"/>
             </BannerTopHome>
+            <section className="cards">
+                <hr className="cards__line"/>
+                <h2 className="cards__title">We're different</h2>
+                <Card title="Snappy Process" text="Our application process can be completed in minutes, not hours. Don't get suck filling in tedious forms.">
+                    <img src={Card01Image} alt="Imagem do card"/>
+                </Card>
+                <Card title="Affordable Prices" text="We don't want you worrying about high monthly costs. Our prices may be low, but we still offer the best coverage possible.">
+                    <img src={Card02Image} alt="Imagem do card"/>
+                </Card>
+                <Card title="People First" text="Our plans aren't full of conditions and clauses to prevent payouts. We make sure you're covered when you need it.">
+                    <img src={Card03Image} alt="Imagem do card"/>
+                </Card>
+            </section>
         </main>
     )
 }
